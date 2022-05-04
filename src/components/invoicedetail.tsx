b313b4e2b2118a9efed8b730d12e5f09acb5c6d0
Hoàng Minh Nhật <hoangminhnhat90@gmail.com>
2022-05-04 18:19:33 +0700

    add router following tut

---
+++ src/components/invoicedetail.tsx
@@ -0,0 +1,28 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import { deleteInvoice, getInvoice } from '../data';
+
+export default function InvoiceDetail() {
+  let param = useParams();
+  let invoice = getInvoice(parseInt(param.invoiceId!, 10));
+  let navigate = useNavigate();
+
+  return (
+    <main style={{ padding: '1rem' }}>
+      <h2>Total Due: {invoice?.amount}</h2>
+      <p>
+        {invoice?.name}: {invoice?.number}
+      </p>
+      <p>Due Date: {invoice?.due}</p>
+      <p>
+        <button
+          onClick={() => {
+            deleteInvoice(invoice?.number!);
+            navigate('/invoices');
+          }}
+        >
+          Delete
+        </button>
+      </p>
+    </main>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Invoices from "./components/invoices";
+import Expenses from "./components/expenses";
+import InvoiceDetail from "./components/invoicedetail";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route
+            index
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>Select an invoice</p>
+              </main>
+            }
+          />
+          <Route
+            path=":invoiceId"
+            element={<InvoiceDetail />}
+          />
+        </Route>
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
